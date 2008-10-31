041a027f03305acd6cb93d05b0af5a66e6fdc551
scoanders78 <scoanders78@2ab58a42-a49d-11dd-a5cd-697bfd0a0cef>
2008-10-31 17:56:22 +0000

    Added scatter plot to plots class.

--- plots.java
+++ plots.java
@@ -1,10 +1,9 @@
-import java.lang.reflect.Array;
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.statistics.*;
+import org.jfree.data.xy.*;
 /**
 * A simple introduction to using JFreeChart. This demo is described in the
 * JFreeChart Developer Guide.
@@ -26,12 +25,28 @@ public class plots {
 	        }else if (data[i] < min){
 	        	min = data[i];
 	        }
-		}System.out.println(min + ", " + max);
+		}
+//		System.out.println(min + ", " + max);
 		mydata.addSeries("freq", data, 20, min, max);
 		// create a chart...
-		JFreeChart chart = ChartFactory.createHistogram(name, "bins", "frequency",
+		JFreeChart chart = ChartFactory.createHistogram(name, "Velocity", "Frequency",
 				mydata, PlotOrientation.VERTICAL, true, true, false);
 		// create and display a frame...
+		ChartFrame frame = new ChartFrame(name, chart);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void scatter(double[][] data, String name){
+		//double[][] mydata = new double[2][10000];  
+		DefaultXYDataset myscater = new DefaultXYDataset(); 
+//		for (int i = 0; i < data.length; i++){
+//			mydata[0][i] = i;
+//			mydata[1][i] = data[i];	
+//		}
+		myscater.addSeries("Velocity", data);
+		JFreeChart chart = ChartFactory.createScatterPlot("Velocity", "Velocity", "Event", 
+				myscater, PlotOrientation.VERTICAL, true, true, false);
 		ChartFrame frame = new ChartFrame(name, chart);
 		frame.pack();
 		frame.setVisible(true);
