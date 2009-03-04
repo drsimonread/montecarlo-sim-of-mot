08c42f2c51d231e4e17316e9ae3ba25c01c15d38
scoanders78 <scoanders78@2ab58a42-a49d-11dd-a5cd-697bfd0a0cef>
2009-03-04 00:48:26 +0000

    Put Simulated Annealing in its own class.  Looks like it works but does not change for different detunings.  IDK.

--- trunk/DataAnalysis.java
+++ trunk/DataAnalysis.java
@@ -1,9 +1,8 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.util.ArrayList;
-import java.util.Random;
 
+import orbital.algorithm.template.SimulatedAnnealing;
 public class DataAnalysis {
 	
 	static final double Lambda_not = 780.24 * Math.pow(10, -9);
@@ -14,100 +13,25 @@ public class DataAnalysis {
 		Configurations data;	
 		SimulationData Sim;
 		ObjectInputStream file;
-		ArrayList<Double> parameters = new ArrayList<Double>();
 //		tempVSdetuning TvsD  = new tempVSdetuning();
 //		double[][]temperatureVSDetuning = new double [2][10];
-		Random r = new Random();
-		double V1, V2, V3, A, B, C, Ti, Tf;
-		Ti = 1;
-		Tf = 100e-6;
-		V1 = Equations.V_calc(Ti);
-		V2 = Equations.V_calc(Tf);
-		V3 = 2*Gamma/k;//should be capture velocity, Foot page 190.
-		A = C = 0.05;
-		B = 0.5;
-		parameters.add(V1);
-		parameters.add(V3);
-		parameters.add(V2);
-		parameters.add(A);
-		parameters.add(B);
-		parameters.add(C);
-		int b = 0;
+		SimulateAnnealing SA;
+		SimulatedAnnealing sa;
 		
+			
 		try{
 			file = new ObjectInputStream(new FileInputStream("simulation_data.dat"));
 			data = (Configurations) file.readObject();
 			file.close();
-
+			
 			for(int a = 0; a < data.size(); a++){
 				Sim = data.get(a);
-				int range = Sim.size();
-				double[][] rawData = new double [2][range];
-				double[][] P_X = new double [2][range];
-				rawData = binnedData(Sim.toArray(), range);				
-				int kmax = 1000, kay = 0;
-				P_X = expCurve(Sim, parameters);
-				double e = sumSqErr(Sim, P_X, rawData), emax = 4e-4, variable, up = 0, down = 0, temp;
-				System.out.println("Initial Error is..." + e);
-/**				
-				s := s0; e := E(s)                           // Initial state, energy.
-				sb := s; eb := e                             // Initial "best" solution
-				k := 0                                       // Energy evaluation count.
-				while k < kmax and e > emax                  // While time remains & not good enough:
-				  sn := neighbour(s)                         //   Pick some neighbour.
-				  en := E(sn)                                //   Compute its energy.
-				  if en < eb then                            //   Is this a new best?
-				    sb := sn; eb := en                       //     Yes, save it.
-				  if P(e, en, temp(k/kmax)) > random() then  //   Should we move to it?
-				    s := sn; e := en                         //     Yes, change state.
-				  k := k + 1                                 //   One more evaluation done
-				return sb                                    // Return the best solution found.
-*/				
-				while(kay < kmax && e > emax){
-					System.out.println("Count = " + kay);
-//					b = r.nextInt(6);
-					for(b = 2; b < parameters.size(); b++){
-					variable = parameters.get(b);
-					temp = parameters.get(b);
-					
-					parameters.set(b, variable + variable * 0.5);
-					P_X = expCurve(Sim, parameters);
-					up = sumSqErr(Sim, P_X, rawData);
-					parameters.set(b, temp);
-					
-					parameters.set(b, variable - variable * 0.5);
-					P_X = expCurve(Sim, parameters);
-					down = sumSqErr(Sim, P_X, rawData);
-					parameters.set(b, temp);
-					
-//					System.out.println("Probability = " + Math.exp(-(e/2)));
-					
-					if(up > down && Math.exp(-down/2) > r.nextDouble()){
-						e = down;
-						parameters.set(b, variable - variable * 0.5);
-						System.out.println("down, Error = " + down);
-					}else if( up < down && Math.exp(-up/2) > r.nextDouble()){
-						e = up;
-						parameters.set(b, variable + variable * 0.5);
-						System.out.println("up, Error = " + up);							
-					}
-					if(e < emax){
-						break;
-					}
-					}
-					kay = kay + 1;
-				}
+				SA = new SimulateAnnealing(Sim);	
+				double [][]ann = new double[2][SA.size()];
+				ann = SA.to2DArray();
+				plots.myLine(ann, "Plot of Fitting Equation", "Velocity", "Probability");
 				
-				plots.myLine(P_X, "Plot of Fitting Equation", "Velocity", "Probability");
-//				plots.myLine(normData, "Normalized Raw Data", "Velocity", "Probability");
-				
-				System.out.println("V1 = " + parameters.get(0));
-				System.out.println("V2 = " + parameters.get(2));
-				System.out.println("V3 = " + parameters.get(1));
-				System.out.println("A = " + parameters.get(3));
-				System.out.println("B = " + parameters.get(4));
-				System.out.println("C = " + parameters.get(5));
-				a = data.size();
+//				a = data.size();
 			}
 //			temperatureVSDetuning = TvsD.toArray();
 //			plots.myLine(temperatureVSDetuning, "Temperature VS. Detuning", "Detuning", "Temperature");
@@ -120,44 +44,7 @@ public class DataAnalysis {
 		}
 	}
 	
-	public static double sumSqErr(SimulationData Sim, double[][] P_X, double[][] rawData){
-		double sumErr = 0;
-		int range = Sim.size();
-		rawData = binnedData(Sim.toArray(), range);
-		
-		for(int i = 0; i < range; i++){
-			sumErr = sumErr + (square(P_X[1][i]) - square(rawData[1][i]/range));
-		}
-		return sumErr;
-	}
-	
-	public static double[][] expCurve(SimulationData Sim, ArrayList<Double> parameters){
-		int range = Sim.size();		
-		double[][] P_X = new double [2][range];
-		double i_min = min(Sim.toArray());
-		double i_max = max(Sim.toArray());
-		double step = (i_max-i_min)/range, A, B, C, V1, V2, V3, p1, p2, p3, v;
-		
-		V1 = parameters.get(0);
-		V2 = parameters.get(2);
-		V3 = parameters.get(1);
-		A = parameters.get(3);
-		B = parameters.get(4);
-		C = A;//parameters.get(5);
-		
-		for(int i = 0; i < Sim.size(); i++){
-			v = i_min + i*step;
-			p1 = A/(Math.sqrt(Math.PI)*V1)*Math.exp(-((v*v)/(V1*V1)));
-			p2 = B/(Math.sqrt(Math.PI)*V1)*Math.exp(-((v*v)/(V2*V2)));
-			p3 = A/(Math.sqrt(Math.PI)*V1)*Math.exp(-((v*v)/(V3*V3)));
-			
-			P_X[0][i] = v;
-			P_X[1][i] = p1 + p2 - p3;	
-		}
-		
-//		System.out.println("The sum of the difference is..." + sumErr);
-		return P_X;
-	}
+
 	
 	public static double max(double[] doubles){
 		double max;
---
+++ trunk/SimulateAnnealing.java
@@ -0,0 +1,161 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Simulated Annealing and the Traveling Salesman
+ * Copyright 2005 by Heaton Research, Inc.
+ * by Jeff Heaton (http://www.heatonresearch.com) 12-2005
+ * -------------------------------------------------
+ * This source code is copyrighted.
+ * You may reuse this code in your own compiled projects.
+ * However, if you would like to redistribute this source code
+ * in any form, you must obtain permission from Heaton Research.
+ * (support@heatonresearch.com).
+ * -------------------------------------------------
+ *
+ * This class implements SimulatedAnnealing.
+ *
+ * -------------------------------------------------
+ * Want to learn more about Neural Network Programming in Java?
+ * Have a look at our e-book:
+ *
+ * http://www.heatonresearch.com/articles/series/1/
+ *
+ * @author Jeff Heaton (http://www.jeffheaton.com)
+ * @version 1.0
+ */
+public class SimulateAnnealing{
+	
+	static final double Lambda_not = 780.24 * Math.pow(10, -9);
+	static final double k = 2 * Math.PI / Lambda_not;
+	static final double Gamma = 6e6;//2 * Math.PI * 3E6;// 1 / tau;
+	private static SimulationData Sim; 
+	private static double[][] rawData;
+	private static double[][] P_X;
+
+	public SimulateAnnealing(SimulationData Sim){
+		SimulateAnnealing.Sim = Sim;
+		
+		ArrayList<Double> parameters = new ArrayList<Double>();
+		int b = 0, range = Sim.size();
+		P_X = new double [2][range];
+		double e, cnt = 1, variable, up = 0, down = 0, temp;
+		double V1, V2, V3, A, B, C, Ti, Tf, Err, T = 10000;
+		Random r = new Random();
+		
+		Ti = 1;
+		Tf = 100e-6;
+		V1 = Equations.V_calc(Ti);
+		V2 = Equations.V_calc(Tf);
+		V3 = (2*Gamma)/k;//should be capture velocity, Foot page 190.
+		A = 0.1;
+		B = A*10;
+		C = A;
+		
+		parameters.add(V1);
+		parameters.add(V2);
+		parameters.add(V3);
+		parameters.add(A);
+		parameters.add(B);
+		parameters.add(C);
+		
+		SimulateAnnealing.P_X = expCurve(parameters);
+		Err = e = sumSqErr();
+//		System.out.println("Initial Error is..." + e);
+
+		while(T > 0.0001){// && e > emax){
+//			System.out.println("Count = " + cnt);
+//			b = r.nextInt(6);
+		for(b = 0; b < 6; b++){
+			variable = parameters.get(b);
+			temp = parameters.get(b);
+			
+			parameters.set(b, variable + variable * 0.5);
+			P_X = expCurve(parameters);
+			up = sumSqErr();
+			parameters.set(b, temp);
+			
+			parameters.set(b, variable - variable * 0.5);
+			P_X = expCurve(parameters);
+			down = sumSqErr();
+			parameters.set(b, temp);
+			
+			if(up > down){
+				Err = down;
+				parameters.set(b, variable + variable * 0.5);
+//				System.out.println("down, Error = " + down);
+			}else{
+				Err = up;
+				parameters.set(b, variable + variable * 0.5);
+//				System.out.println("up, Error = " + up);
+			}
+			
+			if(Err < e){
+				e = Err;
+//				System.out.println("Error = " + e);
+//				System.out.println("");
+			}else if(Math.exp(-(Err/(k*T))) > r.nextDouble()){
+//				System.out.println("Probability = " + (Math.exp(-(Err/(k*T)))));	
+//				System.out.println("Error = " + Err);
+//				System.out.println("");
+				e = Err;							
+			}
+			T = T-T/2;
+		}
+		cnt = cnt+1;
+		}	
+		
+		System.out.println("P(x) = " + parameters.get(3) + " * p(V2 = " + parameters.get(0) + ") + " + parameters.get(4) + " * p(V1 = " + parameters.get(1) + ") + " + parameters.get(5) + "p * (V3 = " + parameters.get(2) + ")");
+//		System.out.println("V1 = " + parameters.get(0));
+		System.out.println("V2 = " + parameters.get(1));
+//		System.out.println("V3 = " + parameters.get(2));
+//		System.out.println("A = " + parameters.get(3));
+		System.out.println("B = " + parameters.get(4));
+//		System.out.println("C = " + parameters.get(5));
+		
+	}
+	
+	private static double sumSqErr(){//SimulationData Sim, double[][] P_X, double[][] rawData){
+		double sumErr = 0;
+		int range = Sim.size();
+		rawData = DataAnalysis.binnedData(Sim.toArray(), range);
+		
+		for(int i = 0; i < range; i++){
+			sumErr = sumErr + DataAnalysis.square(P_X[1][i] - rawData[1][i]/1000);
+		}
+		return Math.abs(sumErr);
+	}
+
+	private static double[][] expCurve(ArrayList<Double> parameters){
+		int range = Sim.size();		
+		double i_min = DataAnalysis.min(Sim.toArray());
+		double i_max = DataAnalysis.max(Sim.toArray());
+		double step = (i_max-i_min)/range, A, B, C, V1, V2, V3, p1, p2, p3, v;
+		
+		V1 = parameters.get(0);
+		V2 = parameters.get(1);
+		V3 = parameters.get(2);
+		A = parameters.get(3);
+		B = parameters.get(4);
+		C = parameters.get(5);
+		
+		for(int i = 0; i < Sim.size(); i++){
+			v = i_min + i*step;
+			p1 = A/(Math.sqrt(Math.PI)*V1)*Math.exp(-((v*v)/(V1*V1)));
+			p2 = B/(Math.sqrt(Math.PI)*V1)*Math.exp(-((v*v)/(V2*V2)));
+			p3 = C/(Math.sqrt(Math.PI)*V1)*Math.exp(-((v*v)/(V3*V3)));
+			
+			P_X[0][i] = v;
+			P_X[1][i] = p1 + p2 - p3;	
+		}
+		return P_X;
+	}
+	
+	public int size(){
+		return P_X.length;
+	}
+	
+	public double[][] to2DArray(){
+		return P_X;
+	}
+}
