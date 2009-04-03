4db826231f116113194af46483f07f734ec66832
scoanders78 <scoanders78@2ab58a42-a49d-11dd-a5cd-697bfd0a0cef>
2009-04-03 17:45:59 +0000

    SA is working.

--- src/edu/smcm/physics/mot/SimulateAnnealing.java
+++ src/edu/smcm/physics/mot/SimulateAnnealing.java
@@ -11,113 +11,114 @@ public class SimulateAnnealing{
 	static final double Lambda_not = 780.24 * Math.pow(10, -9);
 	static final double k = 2 * Math.PI / Lambda_not;
 	static final double Gamma = 6e6;//2 * Math.PI * 3E6;// 1 / tau;
-	private static SimulationData Sim; 
-	private static double[][] P_X;
-	private static double[][] normData;
+	private static double[] fit;
+	private static SimulationData data;
+//	private static int size;
+	private static Random r = new Random();
 	
-	public SimulateAnnealing(SimulationData Sim_){
-		Sim = Sim_;
-		
-		ArrayList<Double> parameters = new ArrayList<Double>();
-		int range = Sim.size();
-		normData = new double [2][range]; 
-		P_X = new double [2][range];
-		double[][]p_x = new double[2][range];
-//		double[][]kde = KDE.SmoothIt(Sim); 
-		double e, cnt = 1, up = 0, down = 0;
-		double V1, V2, V3, A, B, C;
-		LineGraph myLine;
-		Random r = new Random();
-
-		V1 = 20;//Equations.V_calc(1);
-		V2 = 0.05;//Equations.V_calc(100e-6);
-		V3 = 2*(2*Gamma)/k;//should be capture velocity, Foot page 190.		
-		A = 155;
-		B = A*40;
-		C = A;
-
-		parameters.add(V1);
-		parameters.add(V2);
-		parameters.add(V3);
-		parameters.add(A);
-		parameters.add(B);
-		parameters.add(C);
-		
-		normData = DataAnalysis.binnedData(Sim.toArray(), range);	
-		P_X = expCurve(parameters);
-		
-		for(int i  = 0; i < range; i++){
-			p_x[0][i] = P_X[0][i];
-			p_x[1][i] = P_X[1][i];
-		}
-		
-		e = sumSqErr(P_X);
-		
-		System.out.println("Initial Error is..." + e);
-
-		int n = 1;
-		while(cnt < 1000){			
-			for (int i = 0; i < 6; i++){
-				int index = i;//r.nextInt(6);
-				double element = parameters.get(index);
-				double d = (n * 1);
-				
-				parameters.set(index, element + element*d);
-				up = sumSqErr(expCurve(parameters));
-				parameters.set(index, element);
-				
-				parameters.set(index, element - element*d);
-				down = sumSqErr(expCurve(parameters));
-				parameters.set(index, element);
-	
-				if (up >= e && down >= e){
-					n = n + 1;
-				}else if (up < down && up < e){
-					parameters.set(index, element + element*d);
-					e = up;
-				}else if (down < e){
-					parameters.set(index, element - element*d);
-					e = down;
-				}
-				cnt = cnt+1;
-			}
-		}	
-		System.out.println("Final Error........" + e);
-		myLine = new LineGraph("Data, Expected, and Fitted Curves", "Velocity (m/s)", "Probability");
-		myLine.addSeries("Simulated Data", P_X);
-		myLine.addSeries("Expected", p_x);
-		myLine.addSeries("Normalized Data", normData);
-		myLine.plotIT();
-		System.out.println("P(x) = " + parameters.get(3) + " * p(V2 = " + parameters.get(0) + ") + " + parameters.get(4) + " * p(V1 = " + parameters.get(1) + ") + " + parameters.get(5) + "p * (V3 = " + parameters.get(2) + ")");
-		System.out.println("A = " + parameters.get(3));
-		System.out.println("B = " + parameters.get(4));
+	public SimulateAnnealing(SimulationData Sim){
+			SA(Sim);
 	}
 	
-	private static double sumSqErr(double arr[][]){
-		double sumErr = 0;
-		int i_max = arr.length;
+	private ArrayList<Double> SA(SimulationData mySim){
+		data = mySim;
+		int max_iteration = 250;
+		int iteration = 1;
+		double target_error = 1;
+		int cnt = 0;
+		ArrayList<Double> values = new ArrayList<Double>();
+		values = generateParameters();
+		double[][] mydata = DataAnalysis.binnedData(data.toArray(), 100);
+		double error = computeError(values, mydata);
+		ArrayList<Double> best_values = (ArrayList<Double>) values.clone();
+		double best_error = error;
+		double temp_zero = 1000;
+		double temp = 0;
 		
-		for(int i = 0; i < i_max; i++){
-//			if(arr[1][i]*10000 > 0){
-//				System.out.println("Guess distribution is..." + arr[1][i]);
-//			}
-			sumErr = sumErr + DataAnalysis.square(arr[1][i]*10000 - normData[1][i]);
+		while(iteration < max_iteration && error > target_error){
+			ArrayList<Double> next_values = generateParameters();
+			double next_error = computeError(next_values, mydata);
+			
+			if(next_error < best_error){
+				best_values = next_values;
+				best_error = next_error;
+			}
+			temp = temp_zero/iteration;
+			
+			if(exciteProbability(error, next_error, temp) > r.nextDouble()){
+				cnt = cnt + 1;
+//				System.out.println("Accepted new state..." + cnt);
+				values = next_values;
+				error = next_error;
+			}
+			iteration = iteration + 1;
+			System.out.println("Count = " + iteration + ".  Error = " + error + ". Temperature = " + temp);
 		}
+		System.out.println("V1 = " + values.get(0));
+		System.out.println("V2 = " + values.get(1));
+		System.out.println("V3 = " + values.get(2));
+		System.out.println("A = " + values.get(3));
+		System.out.println("B = " + values.get(4));
+		System.out.println("C = " + values.get(5));
+		fit  = expCurve(values);
+		return best_values;
+	}
+	
+	private static ArrayList<Double> generateParameters(){
+		ArrayList<Double> somevalue = new ArrayList<Double>();
+		double V1 = data.getVelocity();
+		double V2 = V1 * r.nextDouble();
+		double V3 = V2 * r.nextDouble();
+		double A = data.getAmplitude();
+		double B = A * r.nextDouble();
+		double C = B * V2 / V3;
+		
+		somevalue.add(V1);
+		somevalue.add(V2);
+		somevalue.add(V3);
+		somevalue.add(A);
+		somevalue.add(B);
+		somevalue.add(C);
+		
+		return somevalue;
+	}
+	
+	private double exciteProbability(double error, double next_error, double temp){
+		double prob = 0;
+		
+		if(error >= next_error){
+			prob = 1.0;
+		}else{
+			prob = Math.exp((error - next_error)/temp);
+			System.out.println("new state Probability = " + prob + ".  e^(" + (error - next_error)/temp + ")");
+		}
+		
+		return prob;
+	}
+	
+	private static double computeError(ArrayList<Double> values, double[][] myData){
+		double sumErr = 0;
+		int i_max = myData.length;
+		double[] the_prediction = expCurve(values);
+		for(int i = 0; i < i_max; i++){
+			sumErr = sumErr + Math.sqrt(DataAnalysis.square(the_prediction[i] - myData[1][i]));
+			} 
 		return sumErr;
 	}
 
-	private static double[][] expCurve(ArrayList<Double> parameters){
-		int range = Sim.size();		
-		double i_min = DataAnalysis.min(Sim.toArray());
-		double i_max = DataAnalysis.max(Sim.toArray());
+	private static double[] expCurve(ArrayList<Double> values){
+		int range = data.size();
+		double[] P_X = new double[range];
+		double i_min = DataAnalysis.min(data.toArray());
+		double i_max = DataAnalysis.max(data.toArray());
 		double step = (i_max-i_min)/range, A, B, C, V1, V2, V3, p1, p2, p3, v;
 		
-		V1 = parameters.get(0);
-		V2 = 1/V1;//parameters.get(1);
-		V3 = parameters.get(2);
-		A = parameters.get(3);
-		B = A*40;//parameters.get(4);
-		C = A;//parameters.get(5);
+		V1 = values.get(0);
+		V2 = values.get(1);
+		V3 = values.get(2);
+		A = values.get(3);
+		B = values.get(4);
+		C = values.get(5);
 		
 		for(int i = 0; i < range; i++){
 			v = i_min + i*step;
@@ -125,18 +126,27 @@ public class SimulateAnnealing{
 			p2 = B/(Math.sqrt(2*Math.PI)*V1)*Math.exp(-((v*v)/(V2*V2)));
 			p3 = C/(Math.sqrt(2*Math.PI)*V1)*Math.exp(-((v*v)/(V3*V3)));
 			
-			P_X[0][i] = v;
-			P_X[1][i] = (p1 + p2 - p3);
-		}
-		
+			P_X[i] = (p1 - p2 + p3);
+		}	
 		return P_X;
 	}
 	
 	public int size(){
-		return P_X.length;
+		return data.size();
 	}
 	
 	public double[][] to2DArray(){
-		return P_X;
+		double i_min = DataAnalysis.min(data.toArray());
+		double i_max = DataAnalysis.max(data.toArray());
+		int range = data.size();
+		double step = (i_max-i_min)/range;
+		double[][]twoD = new double[2][range];
+		
+		for(int i = 0; i < range; i++){
+			twoD[0][i] = i_min + i*step;
+			twoD[1][i] = fit[i];
+		}
+		
+		return twoD;
 	}
 }
